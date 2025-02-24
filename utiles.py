2bdbba949108725c369139f94b7b17a3fac3afb9
AmauryManuelD111 <mauromanuel310@gmail.com>
2025-02-24 12:06:06 +0100

    v17

--- utiles.py
+++ utiles.py
@@ -12,6 +12,13 @@ def json_loader(route):
                     json_list.append(json.load(file))
     return json_list       
 
+def mean(list, key=lambda x: x):
+    new = []
+    for i in list:
+        new.append(key(i))
+    suma = sum(new)
+    total = len(new)
+    return suma/total if total > 0 else None
 
 def obtener_df(json_list):
     for i,v in enumerate(json_list):
@@ -32,4 +39,29 @@ def obtener_df(json_list):
         json_list[i]['contact']['instagram'] = True if v['contact']['instagram'] else False
         json_list[i]['contact']['website'] = True if v['contact']['website'] else False
 
+        for key,val in v['menu'].items():
+            if key:
+                dict_min = {}
+                dict_max = {}
+                value_mean = None
+                try:
+                    dict_min = min(val, key=lambda x: float(x['price']))
+                except: pass
+                json_list[i][f'menu_{key}_min'] = dict_min.get('price', None)
+                try:
+                    dict_max = max(val, key=lambda x: float(x['price']))
+                except: pass
+                json_list[i][f'menu_{key}_max'] = dict_max.get('price', None)
+                try:
+                    value_mean = mean(val, key=lambda x: float(x['price']))
+                except: pass
+                json_list[i][f'menu_{key}_mean'] = value_mean if value_mean else None
+                
+
+        
+
+
     return pd.json_normalize(json_list)
+
+if __name__ == '__main__':
+    df = obtener_df(json_loader('jsons'))
